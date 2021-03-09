624642b519bd77d379e49d0f60494afaa6fc88a1
allanmaeda <allanmaeda@gmail.com>
2021-03-08 23:11:38 -0300

    Initial settings.

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css';
+import GlobalStyles from '../src/styles/global';
 
 function App() {
   return (
+    <>
     <div className="App">
-      
+      <h1>Hello world</h1>
     </div>
+    <GlobalStyles />
+    </>
   );
 }
 
