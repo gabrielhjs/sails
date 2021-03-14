ae4b8866a3db45596ea77ea997ba0d3d3fb6119a
allanmaeda <allanmaeda@gmail.com>
2021-03-14 20:29:45 -0300

    Implemented method to login.

--- src/frontend/src/Context/AuthContext.jsx
+++ src/frontend/src/Context/AuthContext.jsx
@@ -1,18 +1,19 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+
 import useAuth from './hooks/useAuth';
 
 const Context = createContext();
 
-function AuthProvider({ children }) {
-    const {
-        authenticated, loading, handleLogin, handleLogout,
-    } = useAuth();
+const AuthProvider = ({ children }) => {
+  const {
+    authenticated, loading, handleLogin, handleLogout,
+  } = useAuth();
 
-    return (
-        <Context.Provider value={{ loading, authenticated, handleLogin, handleLogout }}>
-            { children}
-        </Context.Provider>
-    );
+  return (
+    <Context.Provider value={{ loading, authenticated, handleLogin, handleLogout }}>
+      {children}
+    </Context.Provider>
+  );
 }
 
 export { Context, AuthProvider };
